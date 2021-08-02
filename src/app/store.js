f3b9c5884a08da1d936be3e6d74c2d4758fbebee
Paulina Korpak <korpak.paulina@gmail.com>
2021-08-02 17:44:00 +0200

    Create food advisor state slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import foodAdvisorReducer from '../features/foodAdvisor/foodAdvisorSlice';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    foodAdvisor: foodAdvisorReducer,
+  },
 });
 
 export default store;
---
+++ src/features/foodAdvisor/foodAdvisorSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+export const foodAdvisorSlice = createSlice({
+  name: 'foodAdvisor',
+  initialState,
+  reducers: {},
+});
+
+export default foodAdvisorSlice.reducer;
