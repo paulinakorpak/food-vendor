7a574cb3bbb50e45f693061cbb8aded7761989df
Paulina Korpak <korpak.paulina@gmail.com>
2021-08-02 19:25:43 +0200

    Handle creatures count

--- src/features/foodAdvisor/components/Form/index.js
+++ src/features/foodAdvisor/components/Form/index.js
@@ -1,20 +1,61 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  setCreatureCount,
+  selectCreatureCount,
+} from '../../foodAdvisorSlice';
 
 function Form() {
+  const dispatch = useDispatch();
+
+  const creaturesCount = useSelector(selectCreatureCount);
+
+  const handleCreatureCountChange = (e) => {
+    const { name, value } = e.currentTarget;
+
+    if (/^\d+$/.test(value) || value === '') {
+      dispatch(setCreatureCount({
+        creature: name,
+        count: parseInt(value, 10),
+      }));
+    }
+  };
+
   return (
     <>
       <div className="creatures">
         <div className="creature">
           <img className="creature-photo" src="images/dwarf.png" alt="dwarf" />
-          <input className="creature-quantity" type="number" min="0" />
+          <input
+            name="dwarf"
+            type="number"
+            min="0"
+            className="creature-quantity"
+            value={creaturesCount.dwarf}
+            onChange={handleCreatureCountChange}
+          />
         </div>
         <div className="creature">
           <img className="creature-photo" src="images/hobbit.png" alt="hobbit" />
-          <input className="creature-quantity" type="number" min="0" />
+          <input
+            name="hobbit"
+            type="number"
+            min="0"
+            className="creature-quantity"
+            value={creaturesCount.hobbit}
+            onChange={handleCreatureCountChange}
+          />
         </div>
         <div className="creature">
           <img className="creature-photo creature-photo-wizard" src="images/wizard.png" alt="wizard" />
-          <input className="creature-quantity" type="number" min="0" />
+          <input
+            name="wizard"
+            type="number"
+            min="0"
+            className="creature-quantity"
+            value={creaturesCount.wizard}
+            onChange={handleCreatureCountChange}
+          />
         </div>
       </div>
       <input
--- src/features/foodAdvisor/foodAdvisorSlice.js
+++ src/features/foodAdvisor/foodAdvisorSlice.js
@@ -1,11 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {};
+const initialState = {
+  creaturesCount: {
+    dwarf: 1,
+    hobbit: 1,
+    wizard: 1,
+  },
+};
 
 export const foodAdvisorSlice = createSlice({
   name: 'foodAdvisor',
   initialState,
-  reducers: {},
+  reducers: {
+    setCreatureCount: (state, action) => {
+      state.creaturesCount[action.payload.creature] = action.payload.count;
+    },
+  },
 });
+
+export const { setCreatureCount } = foodAdvisorSlice.actions;
+
+export const selectCreatureCount = (state) => state.foodAdvisor.creaturesCount;
 
 export default foodAdvisorSlice.reducer;
