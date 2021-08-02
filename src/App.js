f89dcb2b04713c1b84fcc62543b64d829772da15
Paulina Korpak <korpak.paulina@gmail.com>
2021-08-02 20:16:45 +0200

    Show and hide menu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Form from './features/foodAdvisor/components/Form';
 import Menu from './features/foodAdvisor/components/Menu';
+import { selectCreatureCount, selectJourneyLength } from './features/foodAdvisor/foodAdvisorSlice';
 
 function App() {
+  const journeyLength = useSelector(selectJourneyLength);
+  const creaturesCount = useSelector(selectCreatureCount);
+
+  const showMenu = journeyLength > 0 && Object.values(creaturesCount).some((count) => count > 0);
+
   return (
     <div className="container">
       <h1>Food Vendor</h1>
       <Form />
-      <div className="space" />
-      <Menu />
+      {
+        showMenu && (
+          <>
+            <div className="space" />
+            <Menu />
+          </>
+        )
+      }
     </div>
   );
 }
